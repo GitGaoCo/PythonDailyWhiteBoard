63e463f40e388ce8453c0efad2fa087866895ba3
Gary Gao <gao.yng@gmail.com>
2019-09-06 10:45:03 -0700

    B

--- havefunhere.py
+++ havefunhere.py
@@ -3,5 +3,5 @@
 #This is how to convert from string exppression to equation
 #it is very simple just use eval() function and python will return numerical result
 
-
+#change change change
 print eval('- (3 + ( 2 - 1 ) )')
