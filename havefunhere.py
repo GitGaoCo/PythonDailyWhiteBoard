91680a2d7a159fb09efa0b31773ca293214203d1
Gary Gao <gao.yng@gmail.com>
2019-09-08 22:22:37 -0700

    20190908

--- havefunhere.py
+++ havefunhere.py
@@ -14,5 +14,44 @@ def eval_ (expressions):
 print eval_ ('- (3 + ( 2 - 1 ) )')
 
 
+###################################################################################################
+###################################################################################################
+
+#2019-09-08
+#This is a question asked by Facebook to find out the lonest, 2 unique number sequence ina list
+
+#####################################################################################################
+n = [1, 3, 5, 3, 1, 3, 1, 3]
+
+
+
+
+def findSequence(seq):
+	y = []
+	finallist = []
+
+	for i in range (0, len(seq)):
+		for m in range (i, len(seq)):
+			y.append(seq[i:m])
+
+	for value in y:
+		length = len(set(value))
+		if length <= 2:
+			finallist.append(len(value))
+		else:
+			continue	
+	return max(finallist)
+
+
+
+
+
+# there are two ways to express string.
+print "In this sequence, the longest sequence number is %d \n" %(findSequence (n))
+print "In this sequence, the longest sequence number is {} \n".format(findSequence (n))
+
+
+
+
 
 
